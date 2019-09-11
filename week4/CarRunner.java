6c879c87dc2acc3c455c0e2546d9a9a353bee06a
jpburford515 <johnburford6@gmail.com>
2019-09-11 13:48:57 +0000

    Show amount of gas left in tank

--- week4/CarRunner.java
+++ week4/CarRunner.java
@@ -6,5 +6,7 @@ public class CarRunner
         //System.out.println(myMustang);
         myMustang.drive(5.5);
         System.out.println(myMustang.getAmountOfGas());
+        myMustang.drive(100.5);
+        System.out.println(myMustang.getAmountOfGas());
     }
 }
